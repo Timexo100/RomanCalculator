fbf1aeaacc4f67e851b7cb3d8ec6506cf931eba9
Tim <tmuhin@gmail.com>
2021-08-06 11:45:02 +0300

    Refactor calculator.js

--- calculator.js
+++ calculator.js
@@ -1,99 +1,80 @@
-function calculator(string) {
-
-  //This converter from Wiki
-  let converter = {
-
-    arab: [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000],
-    roman: ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'],
-
-    arabToRoman: function(number) {
-      if (!number) return '';
-
-      let ret = '';
-      let i = this.arab.length - 1;
-      while (number > 0) {
-        if (number >= this.arab[i]) {
-          ret += this.roman[i];
-          number -= this.arab[i];
-        } else {
-          i--;
-        }
-      }
-      return ret;
-    },
-    romanToArab: function(str) {
-
-      str = str.toUpperCase();
-      let ret = 0;
-      let i = this.arab.length - 1;
-      let pos = 0;
-      while (i >= 0 && pos < str.length) {
-        if (str.substr(pos, this.roman[i].length) == this.roman[i]) {
-          ret += this.arab[i];
-          pos += this.roman[i].length;
-        } else {
-          i--;
-        }
-      }
-      return ret;
-    }
+class Converter {
+  constructor() {
+    this.arab = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+    this.roman = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
   }
 
-  let calculator = {
+  arabToRoman(number) {
+    if (!number) return "";
 
-    digitSets: {
-      arab: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
-      roman: ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X']
-    },
-
-    operations: {
-      sum: (a, b) => Number(a) + Number(b),
-      difference: (a, b) => Number(a) - Number(b),
-      multiply: (a, b) => Number(a) * Number(b),
-      divide: (a, b) => Math.floor(Number(a) / Number(b))
-    },
-
-    makeOperation(operator, op1, op2) {
-      if (operator == '+') return this.operations.sum(op1, op2);
-      if (operator == '-') return this.operations.difference(op1, op2);
-      if (operator == '*') return this.operations.multiply(op1, op2);
-      if (operator == '/') return this.operations.divide(op1, op2);
-      throw new Error();
-    },
-
-    calculate(expression) {
-
-      pExp = expression.split(" ");
-
-      operator = pExp[1];
-      op1 = pExp[0];
-      op2 = pExp[2];
-
-      if (pExp.length > 3) throw new Error();
-
-      romanDigitSet = this.digitSets.roman;
-      arabDigitSet = this.digitSets.arab;
-
-      if (romanDigitSet.includes(op1) && romanDigitSet.includes(op2)) {
-
-        op1 = converter.romanToArab(op1);
-        op2 = converter.romanToArab(op2);
-
-        result = this.makeOperation(operator, op1, op2);
-        result > 0 ? result = converter.arabToRoman(result) : result = '';
-
-        return result;
+    let ret = "";
+    let i = this.arab.length - 1;
+    while (number > 0) {
+      if (number >= this.arab[i]) {
+        ret += this.roman[i];
+        number -= this.arab[i];
+      } else {
+        i--;
       }
-
-      if (arabDigitSet.includes(op1) && arabDigitSet.includes(op2)) {
-        return this.makeOperation(operator, op1, op2);
-      }
-
-      throw new Error();
     }
+    return ret;
   }
 
-  return String(calculator.calculate(string));
+  romanToArab(str) {
+    str = str.toUpperCase();
+    let ret = 0;
+    let i = this.arab.length - 1;
+    let pos = 0;
+    while (i >= 0 && pos < str.length) {
+      if (str.substr(pos, this.roman[i].length) === this.roman[i]) {
+        ret += this.arab[i];
+        pos += this.roman[i].length;
+      } else {
+        i--;
+      }
+    }
+    return ret;
+  }
 }
 
-module.exports = calculator; // Не трогайте эту строчку
+export default class Calculator {
+  constructor() {
+    this.arab = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+    this.roman = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"];
+    this.converter = new Converter();
+  }
+
+  make(operation, a, b) {
+    switch (operation) {
+      case "+":
+        return +a + +b;
+      case "-":
+        return +a - +b;
+      case "*":
+        return Math.floor(+a * +b);
+      case "/":
+        return Math.floor(+a / +b);
+      default:
+        break;
+    }
+  }
+
+  calculate(expression) {
+    const pExp = expression.split(" ");
+
+    const operator = pExp[1];
+    const a = pExp[0];
+    const b = pExp[2];
+
+    if (pExp.length === 3) {
+      if (this.roman.includes(a) && this.roman.includes(b)) {
+        let aCon = this.converter.romanToArab(a);
+        let bCon = this.converter.romanToArab(b);
+        let result = this.make(operator, aCon, bCon);
+        return result > 0 ? this.converter.arabToRoman(result) : "";
+      } else {
+        return this.make(operator, a, b) + "";
+      }
+    }
+  }
+}
